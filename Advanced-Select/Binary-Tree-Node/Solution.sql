82afb67e68fa82175d4146ff480ee3701d3dad49
Abhishek <abhisheksah1951@gmail.com>
2026-07-05 11:47:33 +0530

    Create Solution.sql

---
+++ Advanced-Select/Binary-Tree-Node/Solution.sql
@@ -0,0 +1,13 @@
+SELECT 
+    N,
+    CASE
+    WHEN P IS NULL THEN 'Root'
+    WHEN N IN(
+        SELECT P
+        FROM BST
+        WHERE P IS NOT NULL
+    ) THEN 'Inner'
+    ELSE 'Leaf'
+    END AS NODETYPE
+    FROM BST
+    ORDER BY N;
